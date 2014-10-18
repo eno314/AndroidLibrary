afd03143918660627c788cffe8042083b51e23a4
Hiroto Kitamura <kirikonknow@gmail.com>
2014-10-18 15:54:50 +0900

    パーサーの型作成

--- library/src/main/java/jp/eno/android/library/parser/GoogleFeedLoadParser.java
+++ library/src/main/java/jp/eno/android/library/parser/GoogleFeedLoadParser.java
@@ -1,22 +1,46 @@
 package jp.eno.android.library.parser;
 
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
 import jp.eno.android.library.model.RssFeed;
 
 /**
+ * GoogleFeedAPIのレスポンスパーサー
  * Created by eno314 on 2014/10/18.
  */
 public class GoogleFeedLoadParser extends Parser<RssFeed> {
 
-    public GoogleFeedLoadParser() {
-
-    }
-
-    RssFeed parseInternal(byte[] body) {
+    RssFeed parseInternal(byte[] body) throws ParseException {
         if (body == null || body.length == 0) {
-
+            throw new ParseException("body is empty...");
         }
 
-        return null;
+        final RssFeed rssFeed;
+
+        try {
+            rssFeed = parseRoot(new String(body, HTTP.UTF_8));
+        }catch (JSONException e) {
+            throw new ParseException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new ParseException(e);
+        }
+
+        return rssFeed;
+    }
+
+    private RssFeed parseRoot(String body) throws JSONException {
+
+        final RssFeed rssFeed = new RssFeed();
+
+        JSONObject rootObject   = new JSONObject( body );
+        JSONObject responseData = rootObject.getJSONObject( "responseData" );
+        JSONObject feed         = responseData.getJSONObject( "feed" );
+
+        return rssFeed;
     }
 
     @Override
